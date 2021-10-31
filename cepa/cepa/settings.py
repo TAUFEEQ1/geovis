7002db633cee0a075618fc9c0c96c973048c9c16
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2021-10-31 06:16:29 +0300

    Update settings.py

--- cepa/cepa/settings.py
+++ cepa/cepa/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'hst_data',
         'USER': 'cepa',
-        'PASSWORD': 'lithium778',
+        'PASSWORD': '#####',
         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
